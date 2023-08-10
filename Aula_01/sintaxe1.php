a40fa519d7d88e0debb3749b7f207183e114c310
5Yago <senaiyago44@gmail.com>
2023-08-10 13:35:39 -0300

    php
    Php exemplos

---
+++ Aula_01/sintaxe1.php
@@ -0,0 +1,7 @@
+<?php
+    // Geração de Texto (string)
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estruturado (com tags, atributos) */
+    echo "<h2>Gerando <span class='destaque'>HTML</span> atráves do PHP. </h2>";
+    echo "<h2> Gerando <span class=\"destaque\"> HTML</span> atráves do PHP. </h2>";
